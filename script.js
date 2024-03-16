39ea435ee0f69da5206c35f0eacea65e5027fe1a
chawanakshata <90598127+chawanakshata@users.noreply.github.com>
2024-03-16 16:19:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+// script.js
+// function submitForm() {
+//     const courseId = document.getElementById("courseId").value;
+//     const taskName = document.getElementById("taskName").value;
+//     const dueDate = document.getElementById("dueDate").value;
+//     const additionalDetails = document.getElementById("additionalDetails").value;
+
+//     const taskData = {
+//         courseId,
+//         taskName,
+//         dueDate,
+//         additionalDetails
+//     };
+
+    // Assuming you have an API endpoint to handle task submission
+//     fetch('/api/tasks', {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify(taskData),
+//     })
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log('Success:', data);
+//         // Handle success, if needed
+//     })
+//     .catch((error) => {
+//         console.error('Error:', error);
+//         // Handle error, if needed
+//     });
+
+// fetch('http://localhost:3000/getstudentTask')
+// .then((res) => res.json())
+// .then((data) => console.log('data',data))
+// .catch((e) => {
+//             console.log(e);
+// })
+
+// }
